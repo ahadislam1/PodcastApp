0fc3de93974c802d99c802f417f56b4eec38fe03
Ahad Islam <34593133+ahadislam1@users.noreply.github.com>
2019-12-17 12:16:00 -0500

    making a podcast controller that can work i hope to god it works please work

---
+++ PodcastApp/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  PodcastApp
+//
+//  Created by Ahad Islam on 12/17/19.
+//  Copyright © 2019 Ahad Islam. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ PodcastApp/PodcastTabBarController.swift
@@ -0,0 +1,35 @@
+//
+//  PodcastTabBarViewController.swift
+//  PodcastApp
+//
+//  Created by Ahad Islam on 12/17/19.
+//  Copyright © 2019 Ahad Islam. All rights reserved.
+//
+
+import UIKit
+
+class PodcastTabBarController: UITabBarController {
+    
+    lazy var searchVC = UINavigationController(rootViewController: SearchViewController())
+    
+    lazy var favoritesVC = UINavigationController(rootViewController: FavoritesViewController())
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        searchVC.tabBarItem = UITabBarItem(title: "Podcasts", image: nil, tag: 0)
+        favoritesVC.tabBarItem = UITabBarItem(title: "Favorites", image: nil, tag: 1)
+        self.viewControllers = [searchVC, favoritesVC]
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
